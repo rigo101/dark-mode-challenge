7d8ac595a9cc2247fc6d4d676cf8a7185140ffe5
Ildiko Rigo <ildiko.bcb@gmail.com>
2021-04-15 21:11:44 +0200

    ThemeSwitcher refactored to func component w/ icon toggle

--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faMoon, faSun} from '@fortawesome/free-solid-svg-icons';
 
-export default class ThemeSwitcher extends React.Component {
+export default function ThemeSwitcher() {
+    const [darkMode, setDarkMode] = useState(false);
 
-    render() {
-        return (
-            <button className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
-                {/* faSun   #FFA500 */}
-            </button>
-        );
-    }
+    const handleClick = () => setDarkMode(prevState => !prevState);
 
+    return (
+        <button className="app__dark-mode-btn icon level-right" onClick={handleClick}>
+            <FontAwesomeIcon
+                icon={darkMode ? faSun : faMoon}
+                color={darkMode ? '#FFA500' : '#4D5B6B'}
+            />
+        </button>
+    );
 }
